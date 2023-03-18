138a23fe9615372c8bdd0657a8a215c2978edc8e
MariYaSobol <pasnova@ukr.net>
2023-03-18 23:14:21 +0200

    first

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          name={friend.name}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.shape),
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {stats.map(data => (
+          <li className={css.item} key={data.id}>
+            <span className={css.label}>{data.label}</span>
+            <span className={css.percentage}>{data.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
